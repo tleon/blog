1888cae28d5e6fef078a3be251a7ea8083abb4d9
tleon <leone.thomas.92@gmail.com>
2019-05-13 09:58:21 +0200

    quete 3 symfony

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class DefaultController extends AbstractController
+{   
+    /**
+     * @route("/", name="app_index")
+    */
+    public function index()
+    {
+      {
+        return $this->render('default.html.twig', [
+                'owner' => 'Thomas',
+        ]); 
+    }
+  }
+}
