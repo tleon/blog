aceefac04ecfd592beb56efc1cee29ac6f770514
tleon <leone.thomas.92@gmail.com>
2019-05-28 22:58:43 +0200

    fixtures

---
+++ src/DataFixtures/ArticleFixtures.php
@@ -0,0 +1,32 @@
+<?php
+namespace App\DataFixtures;
+
+use App\Entity\Article;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Faker;
+
+class ArticleFixtures extends Fixture implements DependentFixtureInterface
+{
+
+  public function load(ObjectManager $manager)
+  {
+    $faker = Faker\Factory::create('fr_FR');
+    $nbRefs = count(CategoryFixtures::CAT) - 1;
+    
+    for($i=0; $i < 1000; $i++){
+      $article = new Article();
+      $article->setTitle($faker->company);
+      $article->setContent($faker->realText($maxNbChars = 200, $indexSize = 2));
+      $article->setCategory($this->getReference('categorie_'.rand(0, $nbRefs)));
+      $manager->persist($article);
+    }
+    $manager->flush();
+  }
+
+  public function getDependencies()
+  {
+      return [CategoryFixtures::class];
+  }
+}
---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,28 @@
+<?php
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+{
+  const CAT = ['php', 'java', 'go', 'javascript', 'Ruby', 'DevOps', 'python'];
+  
+  public function load(ObjectManager $manager)
+  {
+
+    foreach(self::CAT as $k => $v){
+
+      $category = new Category();
+      $category->setName($v);
+      $manager->persist($category);
+      $this->addReference('categorie_' . $k, $category);
+    }
+    $manager->flush();
+  }
+
+  public static function getCat(){
+    return self::CAT;
+  }
+}
