7d4cca784fbf841f491def883737580145bd417f
tleon <leone.thomas.92@gmail.com>
2019-06-09 15:49:31 +0200

    make:user lol

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -39,7 +39,9 @@ class ArticleController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager = $this->getDoctrine()->getManager();
 
-            $article->setSlug($slugify->generate($article->getTitle()));    
+            $article->setSlug($slugify->generate($article->getTitle()));
+            $author = $this->getUser();
+            $article->setAuthor($author);
             $entityManager->persist($article);
             $entityManager->flush();
 
