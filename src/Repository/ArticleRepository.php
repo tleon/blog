a3f29deea28852101bb3a17010aa33db4494e2e6
tleon <leone.thomas.92@gmail.com>
2019-06-18 11:26:02 +0200

    merci fouc

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -14,21 +14,42 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class ArticleRepository extends ServiceEntityRepository
 {
+    /**
+     * ArticleRepository constructor.
+     * @param RegistryInterface $registry
+     */
     public function __construct(RegistryInterface $registry)
     {
         parent::__construct($registry, Article::class);
     }
 
+    /**
+     * @return mixed
+     */
     public function findAllWithCategories()
     {
         $qb = $this->createQueryBuilder('a')
             ->innerJoin('a.category', 'c' )
             ->addSelect('c')
             ->getQuery();
-
         return $qb->execute();
     }
 
+
+    /**
+     * @return mixed
+     */
+    public function findAllWithCategoriesAndTagsAll()
+    {
+        $qb = $this->createQueryBuilder('a')
+            ->leftJoin('a.category', 'c' )
+            ->addSelect('c')
+            ->leftJoin('a.tags', 't')
+            ->addSelect('t')
+            ->orderBy('a.id')
+            ->getQuery();
+        return $qb->execute();
+    }
     // /**
     //  * @return Article[] Returns an array of Article objects
     //  */
